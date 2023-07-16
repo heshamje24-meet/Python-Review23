ee9593c09ac9c82f9cb5229a5135db0ccb7131c9
HeshamJean <heshamjean13@gmail.com>
2023-07-16 17:11:53 +0300

    Update pythonReview.py
    idkk

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,5 @@
 def create_youtube_video(title,descrition):
-	vid={"title":title,"description":descrition:"likes":0,"dilikes":0,"comments":{username:""}}
+	vid={"title":title,"description":descrition,"likes":0,"dilikes":0,"comments":{username:""}}
 	return vid
 def like(youtube_video):
 	if  "likes" in youtube_video:
@@ -15,4 +15,4 @@ def add_comment(youtube_video,username,comment_text):
 	return comments
 
 
-new={katia is dumb,idkk}
+new={katia dumb,idkk}
