0a2b91d6ba21df747e58a49f6904bc309dca3ae3
HeshamJean <heshamjean13@gmail.com>
2023-07-16 17:17:37 +0300

    Update pythonReview.py
    yupp

--- pythonReview.py
+++ pythonReview.py
@@ -4,15 +4,15 @@ def create_youtube_video(title,descrition):
 def like(youtube_video):
 	if  "likes" in youtube_video:
 		likes+=1
-	    return youtube_video
+	return youtube_video
 
 def dislike(youtube_video):
 	if  "dislikes" in youtube_video:
 		dislikes+=1
-	    return youtube_video
+	return youtube_video
 def add_comment(youtube_video,username,comment_text):
 	comments={"username":comment_text}
 	return comments
 
 
-new={katia dumb,idkk}
+new={"katia is dumb","idkk"}
