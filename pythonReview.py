607ab3e402bfa3b5539da6dcd6a09d3a99b1c3d4
HeshamJean <heshamjean13@gmail.com>
2023-07-16 16:57:48 +0300

    Create pythonReview.py
    idkk

---
+++ pythonReview.py
@@ -0,0 +1,18 @@
+def create_youtube_video(title,descrition)
+	vid={"title":title,"description":descrition:"likes":0,"dilikes":0,"comments":{username:""}}
+	return vid
+def like(youtube_video)
+	if  "likes" in youtube_video:
+		likes+=1
+	    return youtube_video
+
+def dislike(youtube_video)
+	if  "dislikes" in youtube_video:
+		dislikes+=1
+	    return youtube_video
+def add_comment(youtube_video,username,comment_text)
+	comments={"username":comment_text}
+	return comments
+
+
+new={katia is dumb,idkk}
